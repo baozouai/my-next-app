8b477c671fc8a6b26a86464c0bfd7177af411080
baozouai <baozouai@gmail.com>
2024-01-19 19:35:52 +0800

    feat: add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,15 @@ export default function Home(){
           <UserButton afterSignOutUrl="/"/>
         </div>
       </header>
+      <div className='flex items-center justify-center m-4'>
+        <main className='flex flex-col items-center justify-center w-full border-x-2 md:w-9/12 lg:w-6/12'>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis distinctio magnam voluptatibus ratione voluptatem quis commodi sit aut? Sequi placeat ratione ut facilis. Ducimus atque, sequi impedit dolor quam molestias!
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi odio vel, perferendis aspernatur inventore ea eaque dicta veniam iure, praesentium nemo autem. Asperiores minima, consequatur modi iure corporis tempora velit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In enim, dolorem recusandae voluptatum minima ratione amet deserunt ipsam maxime, provident id, quibusdam similique sed accusantium harum asperiores quam nobis? Autem!</p>
+        </main>
+      </div>
     </div>
   );
 }
